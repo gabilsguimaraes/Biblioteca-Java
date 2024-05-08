b0d334aa6e4a6fa7f301256e193732350140a100
gabilsguimaraes <163374193+gabilsguimaraes@users.noreply.github.com>
2024-05-07 22:26:59 -0300

    Update FileManager.java

--- biblioteca/FileManager.java
+++ biblioteca/FileManager.java
@@ -1,20 +1,19 @@
 package biblioteca;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import biblioteca.modelos.Livro;
 
 public class FileManager {
-    public static void salvarLivros(List<Livro> livros, String nomeArquivo) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
+    public static void salvarLivros(List<Livro> livros, String caminhoArquivo) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(caminhoArquivo))) {
             for (Livro livro : livros) {
-                writer.write(livro.toString());
-                writer.newLine(); 
+                writer.println(livro.toString());
             }
-            System.out.println("Dados salvos com sucesso em " + nomeArquivo);
+            System.out.println("Dados salvos com sucesso em " + caminhoArquivo);
         } catch (IOException e) {
             System.out.println("Erro ao salvar dados: " + e.getMessage());
         }
